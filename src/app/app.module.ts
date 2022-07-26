b27f06da2bc6bef80d7a6541d6469f7c4b0cb5b6
Your Name <you@example.com>
2022-07-26 12:24:36 +0530

    Folder Strucutre

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/about/about.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { WorkComponent } from './components/work/work.component';
+import { TestimonialComponent } from './components/testimonial/testimonial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    SkillsComponent,
+    WorkComponent,
+    TestimonialComponent
   ],
   imports: [
     BrowserModule,
